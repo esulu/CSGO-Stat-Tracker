468e5e3c87912f463c68e09e28b02baa4fddab61
Eren Sulutas <erensulu11@gmail.com>
2020-04-25 11:37:38 -0400

    added environment variables

--- server.js
+++ server.js
@@ -2,3 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
+const app = express();
+
+app.listen(5000, () => {
+    console.log('Listening on port 5000');
+});
+
