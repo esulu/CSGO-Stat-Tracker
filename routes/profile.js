df0da544afd436b3f501f05cf5a16fe1afda0cf8
Eren Sulutas <erensulu11@gmail.com>
2020-04-27 11:11:38 -0400

    typo

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
-router.get('/:id', async (req, res) => { 
+router.get('/:userId', async (req, res) => { 
     try {
         const headers = {
             'TRN-Api-Key': process.env.TRACKER_API_KEY
